06c7b68f5f2bc0adce4d34f36d6c6ba36be46d37
tamnguyen <nhmtam.c3tqcap.a3@gmail.com>
2021-05-17 17:21:24 +0700

    load san pham

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,10 +1,20 @@
+const HomeModel = require('../models/client/HomeModel');
+const conn = require('../models/config/connect');
+
 class HomeController {
     //[GET] /
-    index(req, res, next) {
-        res.render('client/home/index',{
-            title: 'donghothoitrang',
+    index(req, res) {
+        let listSP = [];
+        HomeModel.loadSanPham().then(result =>{
+            listSP = result;
+            res.render('client/home/index', {
+                title: 'donghothoitrang',
+                index: listSP})
+        }).catch(err =>{
+            res.render('client/home/index');
         });
     }
+    
 }
 
 module.exports = new HomeController();
