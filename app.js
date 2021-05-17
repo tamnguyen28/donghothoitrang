42eb5ce7aa7dd24c7763e7c82b994a74fdbd24fc
tamnguyen <nhmtam.c3tqcap.a3@gmail.com>
2021-05-17 17:55:02 +0700

    second commit

--- app.js
+++ app.js
@@ -21,6 +21,6 @@ route(app);
 
 //Start the server
 const port = 3000;
-app.listen(port, function(){
+app.listen(process.env.PORT || port, function(){
     console.log('Server started on port ' + port);
 });
