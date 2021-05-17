16f09f8624f26e40754d69226cc7910803f418ca
tamnguyen <nhmtam.c3tqcap.a3@gmail.com>
2021-05-17 22:32:09 +0700

    4 commit

--- app.js
+++ app.js
@@ -16,11 +16,12 @@ app.set('view engine', 'ejs');
 //Set public folder
 // app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('public'))
-//Routes init
-route(app);
 
 //Start the server
 const port = 3000;
 app.listen(process.env.PORT || port, function(){
     console.log('Server started on port ' + port);
 });
+
+//Routes init
+route(app);
