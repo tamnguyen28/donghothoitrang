ecbf03c55097c9a14f3fa3cac5c9dae015bffd0b
tamnguyen <nhmtam.c3tqcap.a3@gmail.com>
2021-06-09 22:51:33 +0700

    chuc nang tim kiem

--- controllers/TimKiemController.js
+++ controllers/TimKiemController.js
@@ -3,9 +3,14 @@ const timkiemModel = require('../models/TimKiemModel');
 class TimKiemController{
 
     search(req, res) {
-        res.render('client/timkiem/timkiem', {
-            title: 'Tìm kiếm',
-            giohangs: (req.session && req.session.giohang ? req.session.giohang: [] ),
+        timkiemModel.search(req.query.search).then(function(result){
+            res.render('client/timkiem/timkiem', {
+                title: 'Tìm kiếm',
+                search: result,
+                giohangs: (req.session && req.session.giohang ? req.session.giohang: [] ),
+            });
+        }).catch(err => {
+            console.log(err);
         });
     }
 }
--- models/TimKiemModel.js
+++ models/TimKiemModel.js
@@ -1,6 +1,20 @@
 const conn = require('./config/connect');
 class TimKiemModel{
-
+    search(keyword){
+        return new Promise(function(resolve, reject){
+            let data = keyword+'%';
+            let querysearch = `SELECT sanpham.* 
+                               FROM sanpham JOIN thuonghieu on sanpham.id_math = thuonghieu.math 
+                               WHERE sanpham.tensp like N? OR thuonghieu.tenth like N?`;
+            conn.query(querysearch, [data, data] ,function(err, result){
+                if(err){
+                    reject(err);
+                }else{
+                    resolve(result);
+                }
+            })
+        })
+    }
 }
 
 module.exports = new TimKiemModel();
