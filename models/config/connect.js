fcd486089c7e997eacd37f4bd8e27b1fe21c4190
tamnguyen <nhmtam.c3tqcap.a3@gmail.com>
2021-06-09 11:06:17 +0700

    change infor connect 2

--- models/config/connect.js
+++ models/config/connect.js
@@ -1,6 +1,6 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'db4free',
+  host     : 'db4free.net',
   user     : 'tamnguyen',
   password : 'Nguyenhuynhminhtam1@',
   database : 'donghothoitrang',
