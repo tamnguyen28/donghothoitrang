569dc8c94bdb34e7411954af8a4bec043ec506b3
tamnguyen <nhmtam.c3tqcap.a3@gmail.com>
2021-06-12 15:40:49 +0700

    merge client

--- models/config/connect.js
+++ models/config/connect.js
@@ -1,9 +1,9 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'db4free.net',
-  user     : 'tamnguyen',
-  password : 'Nguyenhuynhminhtam1@',
-  database : 'donghothoitrang',
+  host     : 'sql6.freesqldatabase.com',
+  user     : 'sql6418648',
+  password : '8Dg17tT43n',
+  database : 'sql6418648',
   port : 3306
 });
 module.exports = connection;
