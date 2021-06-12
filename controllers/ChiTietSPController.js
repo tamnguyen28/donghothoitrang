b8ffa10887a269c128950c536692cf3d75ef7057
tamnguyen <nhmtam.c3tqcap.a3@gmail.com>
2021-06-12 19:46:05 +0700

    fix bug

--- controllers/ChiTietSPController.js
+++ controllers/ChiTietSPController.js
@@ -7,31 +7,18 @@ class ChiTietSPController{
 
         let idProduct = req.query.id;
 
-        chitietspModel.loadChitiet(idProduct).then(function(listct){
-            chitietspModel.loadBinhLuan(idProduct).then(function(result){
-                bl = result[0];
-                chids = result[1];
-                customFleid();
-                res.render('client/chitietsp/chitietsp',{
-                    title: 'Chi tiết sản phẩm',
-                    indexct: listct,
-                    giohangs: (req.session && req.session.giohang ? req.session.giohang: [] )
-                });
-            })
+        chitietspModel.loadChitiet(idProduct).then(function (listct) {
+
+            res.render('client/chitietsp/chitietsp', {
+                title: 'Chi tiết sản phẩm',
+                indexct: listct,
+                giohangs: (req.session && req.session.giohang ? req.session.giohang : [])
+            });
+
         }).catch(err => {
             console.log(err);
         });
     }
-}
-
-function customFleid(){
-    for(let i = 0; i < bl.length; i++){
-        for(let j = 0; j < chids.length; j++){
-            // bl[i].childsbl = chids[0];
-        }
-    }
-
-//    console.log(bl);
 }
 
 module.exports = new ChiTietSPController();
