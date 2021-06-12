0fad1ab53b74a58f5c3d7f8b01d62988b1daaf71
tamnguyen <nhmtam.c3tqcap.a3@gmail.com>
2021-06-12 19:52:01 +0700

    fix info connect

--- models/config/connect.js
+++ models/config/connect.js
@@ -1,16 +1,9 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-<<<<<<< HEAD
   host     : 'sql6.freesqldatabase.com',
   user     : 'sql6418648',
   password : '8Dg17tT43n',
   database : 'sql6418648',
-=======
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'donghothoitrang',
->>>>>>> client
   port : 3306
 });
 module.exports = connection;
