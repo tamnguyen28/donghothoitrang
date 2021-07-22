f20b14b779cc78acd373a138ba15de5b70b1d03a
tamnguyen <nhmtam.c3tqcap.a3@gmail.com>
2021-07-22 22:49:40 +0700

    edit email

--- models/configmail/configmail.js
+++ models/configmail/configmail.js
@@ -1,6 +1,5 @@
 // main.js
 var nodemailer = require('nodemailer');
-// const sgMail = require('@sendgrid/mail');
 
 const emailDefaul = 'nhmtam.c3tqcap.a3@gmail.com';
 
@@ -17,21 +16,6 @@ var transporter = nodemailer.createTransport(option);
 
 
 const API_KEY = `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`;
-
-
-// module.exports = {
-//     sendmail: function(emailTo, subject, text ){    
-//         // console.log(emailTo);  
-//         sgMail.setApiKey(API_KEY);
-//         const message = {
-//             from: 'Đồng hồ thời trang <nhmtam.c3tqcap.a3@gmail.com>',
-//             to: emailTo,
-//             subject: subject,
-//             text: text,        
-//         }
-//         sgMail.send(message).then(res => console.log("Email sendt...")).catch(error => console.log(error.message));
-//     }
-// }
 
 module.exports = {
     sendmail: function(emailTo, subject, text ){
