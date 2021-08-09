0bd3cb5f52024fc40399b736d369fe67354f3900
minhtho288 <minhtho288>
2021-08-09 22:32:22 +0700

    sua thong bao thanh modal bootstrap va sua trang chu

--- controllers/AdquenmatkhauController.js
+++ controllers/AdquenmatkhauController.js
@@ -4,33 +4,33 @@ const mail = require('../models/configmail/configmail');
 const AdquenmatkhauModel = require('../models/AdquenmatkhauModel');
 
 let emailUser = '';
-let uuidSave = ''; 
-class AdquenmatkhauController{
-    index(req, res){
+let uuidSave = '';
+class AdquenmatkhauController {
+    index(req, res) {
         let mess = req.query.mess;
         res.render('admin/adquenmatkhau/email', {
             title: 'Quên mật khẩu',
-            role:'',
+            role: '',
             mess: mess ? mess : -1,
             tennv: req.cookies.admin ? req.cookies.admin.tennv : '',
-            manv: req.cookies.admin ? req.cookies.admin.manv: 0
+            manv: req.cookies.admin ? req.cookies.admin.manv : 0
         })
     }
-    
+
     sendMail(req, res) {
-        
+
         let email = req.body.emailUser;
         emailUser = email;
         uuidSave = uuid();
 
-        AdquenmatkhauModel.checkMailExist(emailUser).then(function(result){
-            if(result == true){
-                setTimeout(function(){
+        AdquenmatkhauModel.checkMailExist(emailUser).then(function (result) {
+            if (result == true) {
+                setTimeout(function () {
                     uuidSave = '';
-                },120000);
-        
+                }, 120000);
+
                 let link = `${req.protocol}://${req.hostname}:3000/admin/quenmatkhau/matkhaumoi?token=${uuidSave}`;
-        
+
                 let contentRegister = `Kính chào quý khách
                 FULLTIME đã nhận được yêu cầu thay đổi mật khẩu của quý khách.
                 Xin hãy click vào link bên dưới để đổi mật khẩu.
@@ -42,44 +42,44 @@ class AdquenmatkhauController{
                 - Giờ làm việc: 8:00 - 22:00 (Tất cả các ngày bao gồm cả Lễ Tết)
         
                 - Email hỗ trợ: hoidap@fulltime.com`
-        
+
                 mail.sendmail(email, 'Yêu cầu thay đổi mật khẩu', contentRegister);
-        
+
                 res.redirect(`/admin/message?statusCode=1&emailUser=${encodeURIComponent(email)}`);
-            }else{
+            } else {
                 res.redirect('/admin/quenmatkhau/email?mess=0');
 
             }
-        }).catch(function(error){
+        }).catch(function (error) {
             console.log(error);
         })
     }
 
-    matkhau(req, res){
-        let uuid =  req.query.token;
+    matkhau(req, res) {
+        let uuid = req.query.token;
 
-        if(uuid === uuidSave){
+        if (uuid === uuidSave) {
             res.render('admin/adquenmatkhau/matkhaumoi', {
                 title: 'Đặt lại mật khẩu',
-                role:'',
+                role: '',
                 tennv: req.cookies.admin ? req.cookies.admin.tennv : '',
-                manv: req.cookies.admin ? req.cookies.admin.manv: 0,
+                manv: req.cookies.admin ? req.cookies.admin.manv : 0,
             });
-        }else{
+        } else {
             res.send('Token hết hạn');
-        }  
+        }
     }
 
-    updateMatKhau(req, res){
-        let pass = req.body.password; 
-      
-        AdquenmatkhauModel.updateMatKhau(md5(pass), emailUser).then(function(result){
-            if(result == true){
+    updateMatKhau(req, res) {
+        let pass = req.body.password;
+
+        AdquenmatkhauModel.updateMatKhau(md5(pass), emailUser).then(function (result) {
+            if (result == true) {
                 uuidSave = '';
                 res.redirect('/admin/login');
             }
-        }).catch(function(error){
-            console.log(error);
+        }).catch(function (error) {
+            // console.log(error);
         })
     }
 }
--- controllers/AdsanphamController.js
+++ controllers/AdsanphamController.js
@@ -2,12 +2,12 @@ const AdsanphamModel = require("../models/AdsanphamModel");
 const conn = require('../models/config/connect');
 
 let oldFileName = null;
-let oldSp = null; 
+let oldSp = null;
 class AdsanphamController {
     //[GET] /admin
     index(req, res) {
 
-        if(!req.cookies.admin){
+        if (!req.cookies.admin) {
             return res.redirect('/admin/login')
         }
         let page = req.query.page;
@@ -15,10 +15,10 @@ class AdsanphamController {
         let listsp = [];
 
         let loadSP = [];
-            AdsanphamModel.loadSanPham().then(resultPage => {
-                listsp = resultPage;
-                let soluongtrang = listsp.length / 10;
-                AdsanphamModel.loadAllSanPham(vitribatdaulay).then(function(result){
+        AdsanphamModel.loadSanPham().then(resultPage => {
+            listsp = resultPage;
+            let soluongtrang = listsp.length / 10;
+            AdsanphamModel.loadAllSanPham(vitribatdaulay).then(function (result) {
                 loadSP = result;
                 res.render('admin/adsanpham/sanpham', {
                     title: 'sanpham',
@@ -26,34 +26,34 @@ class AdsanphamController {
                     role: req.cookies.admin.id_maloainv,
                     mess: req.query.mess && req.query.mess == 1 ? req.query.mess : '',
                     tennv: req.cookies.admin ? req.cookies.admin.tennv : '',
-                    manv: req.cookies.admin ? req.cookies.admin.manv: 0,
+                    manv: req.cookies.admin ? req.cookies.admin.manv : 0,
                     idnv: req.cookies.admin.manv,
                     pageNumber: Math.ceil(soluongtrang)
                 });
-            }).catch((err)=>{
+            }).catch((err) => {
                 console.log(err);
             });
-    
-            }).catch(err => {
-                res.render('admin/home/index',{
-                    title: 'sanpham',
-                    role: req.cookies.admin.id_maloainv,
-                    tennv: req.cookies.admin ? req.cookies.admin.tennv : '',
-                    manv: req.cookies.admin ? req.cookies.admin.manv: 0,
-                    idnv: req.cookies.admin.manv
-                });
+
+        }).catch(err => {
+            res.render('admin/home/index', {
+                title: 'sanpham',
+                role: req.cookies.admin.id_maloainv,
+                tennv: req.cookies.admin ? req.cookies.admin.tennv : '',
+                manv: req.cookies.admin ? req.cookies.admin.manv : 0,
+                idnv: req.cookies.admin.manv
             });
+        });
     }
     //[GET] /admin/sanpham/create
     create(req, res, next) {
         AdsanphamModel.loadDanhMuc().then(function (result) {
 
             AdsanphamModel.loadThuongHieu().then(function (resulttt) {
-                res.render('admin/adsanpham/create', { 
+                res.render('admin/adsanpham/create', {
                     danhmucs: result, thuonghieus: resulttt,
                     role: req.cookies.admin.id_maloainv,
                     tennv: req.cookies.admin ? req.cookies.admin.tennv : '',
-                    manv: req.cookies.admin ? req.cookies.admin.manv: 0,
+                    manv: req.cookies.admin ? req.cookies.admin.manv : 0,
                     idnv: req.cookies.admin.manv
                 });
             }).catch(function (err) {
@@ -84,12 +84,12 @@ class AdsanphamController {
         // console.log(hinhanh);
         let createSP = [];
         AdsanphamModel.createSanPham(newSp).then(resultProduct => {
-            AdsanphamModel.insertCollectImag(resultProduct,req.files).then(function(result){
+            AdsanphamModel.insertCollectImag(resultProduct, req.files).then(function (result) {
                 res.redirect('/admin/sanpham?page=1');
-            }).catch(function(error){
+            }).catch(function (error) {
 
             })
-            
+
         }).catch(err => {
             // console.log(err);
             res.render('/admin');
@@ -102,22 +102,22 @@ class AdsanphamController {
         AdsanphamModel.getProductById(idsanpham).then(function (result) {
             AdsanphamModel.loadThuongHieu().then(function (resulttt) {
                 AdsanphamModel.loadDanhMuc().then(function (resultdm) {
-                    AdsanphamModel.getImageProduct(idsanpham).then(function(resultBohinhanh){
+                    AdsanphamModel.getImageProduct(idsanpham).then(function (resultBohinhanh) {
                         oldFileName = result[0].hinhanh
                         oldSp = result;
-                       
+
                         res.render('admin/adsanpham/update', {
                             product: result[0], thuonghieus: resulttt, danhmucs: resultdm,
                             role: req.cookies.admin.id_maloainv,
                             tennv: req.cookies.admin ? req.cookies.admin.tennv : '',
-                            manv: req.cookies.admin ? req.cookies.admin.manv: 0,
+                            manv: req.cookies.admin ? req.cookies.admin.manv : 0,
                             idnv: req.cookies.admin.manv,
-                            bohinhanh:resultBohinhanh
+                            bohinhanh: resultBohinhanh
                         });
-                    }).catch(function(error){
+                    }).catch(function (error) {
 
                     })
-                   
+
                 })
             }).catch(function (erro) {
 
@@ -127,16 +127,16 @@ class AdsanphamController {
         });
     }
     // [POST] /admin/sanpham/store (update)
-     storeupdate(req, res) {
+    storeupdate(req, res) {
         let masp = req.body['masp'];
         let tensp = req.body['tensp'];
         let giatien = Number(req.body['giatien']);
         let trangthai = Number(req.body['trangthai']);
-        let hinhanh = (req.files[0] ? req.files[0].filename: oldFileName);
+        let hinhanh = (req.files[0] ? req.files[0].filename : oldFileName);
         let mota = req.body['mota'];
         let thuonghieu = Number(req.body['id_math']);
-        let loaisanpham = req.body['id_maloai'] == -1 ? oldSp[0].id_maloai : req.body['id_maloai'] ;
-            
+        let loaisanpham = req.body['id_maloai'] == -1 ? oldSp[0].id_maloai : req.body['id_maloai'];
+
         let newSp = {
             masp: masp,
             name: tensp,
@@ -148,37 +148,37 @@ class AdsanphamController {
             type: loaisanpham
         }
         AdsanphamModel.updateSanPham(newSp).then(resultProduct => {
-            AdsanphamModel.getImageProduct(masp).then(function(reusltImag){
-                AdsanphamModel.updateCollectImag(masp,req.files,reusltImag).then(function(result){
+            AdsanphamModel.getImageProduct(masp).then(function (reusltImag) {
+                AdsanphamModel.updateCollectImag(masp, req.files, reusltImag).then(function (result) {
                     res.redirect('/admin/sanpham?page=1');
-                }).catch(function(error){
+                }).catch(function (error) {
                     console.log(error);
                 })
-            }).catch(function(error){
+            }).catch(function (error) {
 
             })
-            
+
         }).catch(err => {
             res.render('/admin');
         });
     }
 
-    deleteSanpham(req, res){
+    deleteSanpham(req, res) {
         let idsanpham = req.query.id;
-        AdsanphamModel.getOrderOfProduct(idsanpham).then(function(result){
-            if(result.length == 0){
-                AdsanphamModel.deleteSanPham(idsanpham).then(function(resultDeleteProduct){
+        AdsanphamModel.getOrderOfProduct(idsanpham).then(function (result) {
+            if (result.length == 0) {
+                AdsanphamModel.deleteSanPham(idsanpham).then(function (resultDeleteProduct) {
                     res.redirect('/admin/sanpham?page=1');
-                }).catch(function(error){
+                }).catch(function (error) {
                     res.redirect('/admin/sanpham?page=1');
                 })
-            }else{
+            } else {
                 res.redirect('/admin/sanpham?page=1&mess=1');
             }
-        }).catch(function(error){
-           
+        }).catch(function (error) {
+
         })
-        
+
     }
 }
 
